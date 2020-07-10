38df3a35492188ea24d4a62af3c82c7de10a1a4a
RipudamanKaushikDal <Ripudaman.kaushik@dal.ca>
2020-07-10 02:20:30 -0300

    Designed the search page for manga app

---
+++ Manga Reader/Manga Reader/Manga Reader/SearchPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using MangaScrapeLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MangaReader
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SearchPage : ContentPage
+    {
+        public List<SearchViewModel> searchResults = new List<SearchViewModel>();
+
+        public SearchPage()
+        {
+            InitializeComponent();
+        }
+        private async void searchBar_SearchButtonPressed(object sender, EventArgs e)
+        {
+            var  repo = Repositories.AllRepositories[2];
+            var series = await repo.SearchSeriesAsync(searchBar.Text);
+            foreach (var item in series)
+            {
+                searchResults.Add(new SearchViewModel {Name=item.Title, Cover=item.CoverImageUri, Author=item.Author });
+            }
+            searchList.ItemsSource = searchResults;
+        }
+    }
+}
---
+++ Manga Reader/Manga Reader/Manga Reader/SearchViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MangaReader
+{
+    public class SearchViewModel
+    {
+        public string Name { get; set; }
+
+        public string Author { get; set; }
+
+        public Uri Cover { get; set; }
+    }
+}
